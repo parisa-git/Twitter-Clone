05e983ed5244c9141d2aea10b86319aa0708c33f
parisa <mbkparisa@gmail.com>
2022-07-13 10:18:53 +0430

    get post data from db and show them in post component

--- components/Feed.js
+++ components/Feed.js
@@ -1,30 +1,27 @@
 import { SparklesIcon } from '@heroicons/react/outline'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
 import Input from './Input'
 import Post from './Post';
 
 const Feed = () => {
-    const posts = [
-        {
-            id: "1",
-            name: "parisa mobaraki",
-            username: "parisa mbk",
-            userimag: "https://lh3.googleusercontent.com/a-/AFdZucogHTbyi6uYA7NJHct3AjBC_ZcwCGvY2A1Nqq4S=s96-c-rg-br100",
-            img: "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-            text: "nice pic!!",
-            timestamp: "2 days ago"
-        },
-        {
-            id: "2",
-            name: "parisa mobaraki",
-            username: "parisa mbk",
-            userimag: "https://lh3.googleusercontent.com/a-/AFdZucogHTbyi6uYA7NJHct3AjBC_ZcwCGvY2A1Nqq4S=s96-c-rg-br100",
-            img: "https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-            text: "fantastic !!",
-            timestamp: "2 days ago"
-        }
-    ];
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(collection(db, "posts"), orderBy("timestamp", "desc")),
+                (snapshot) => {
+                    setPosts(snapshot.docs);
+                }
+            ),
+        []
+    );
+
     return (
-        <div className='xl:ml-[330px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow  w-full'>
+        <div className='xl:ml-[330px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow '>
             <div className='flex sticky py-2 px-3 bg-white top-0 z-50 border-b border-gray-200'>
                 <h2 className='cursor-pointer text-lg sm:text-xl  font-bold'>Home</h2>
                 <div className='hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9'>
--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,5 @@
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from "@heroicons/react/outline"
+import Moment from "react-moment"
 
 
 const Post = ({ post }) => {
@@ -6,31 +7,35 @@ const Post = ({ post }) => {
         <div className="flex p-3 cursor-pointer border-b border-gray-200">
             {/* user image */}
             <img className="w-10 h-10 rounded-full cursor-pointer hover:brightness-95 mr-4"
-                src={post.userimag}
+                src={post.data().userImage}
                 alt="user-img"
             />
 
             {/* right side  */}
-            <div>
+            <div className="w-full">
                 {/* header */}
 
                 <div className="flex justify-between items-center">
                     {/* user info  */}
                     <div className="flex items-center gap-1 whitespace-nowrap">
-                        <h4 className="xl:text-lg text-sm font-bold text-gray-700 hover:underline">{post.name}</h4>
-                        <span className="text-sm text-gray-500">@{post.username} -</span>
-                        <span className="text-sm text-gray-500 hover:underline">{post.timestamp}</span>
+                        <h4 className="xl:text-lg text-sm font-bold text-gray-700 hover:underline">{post.data().name}</h4>
+                        <span className="text-sm text-gray-500">@{post.data().username} -</span>
+                        <span className="text-sm text-gray-500 hover:underline">
+                            <Moment fromNow>
+                                {post?.timestamp?.toDate()}
+                            </Moment>
+                        </span>
                     </div>
 
-                    {/* dot icon  */} 
+                    {/* dot icon  */}
                     <DotsHorizontalIcon className="xl:h-11 h-5 hoverEffect hover:bg-sky-100 hover:text-sky-500 " />
                 </div>
 
                 {/* post text */}
-                <p className="text-gray-700 text-lg sm:text-sm mb-2">{post.text}</p>
+                <p className="text-gray-700 text-lg sm:text-sm mb-2">{post.data().text}</p>
 
                 {/* post img  */}
-                <img className="rounded-2xl mr-2" src={post.img} />
+                <img className="rounded-2xl mr-2 w-full  h-auto" src={post.data().image} />
 
                 {/* icons  */}
                 <div className="flex justify-between text-gray-500 mt-2 p-2">
